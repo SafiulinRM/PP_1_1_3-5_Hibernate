f75267bc490cb62138ad69cd64a3586eaec20c72
safiulinrm <zloyangel666999@gmail.com>
2023-01-22 21:44:44 +0300

    feat: "Клас для получения подключения"

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getMySQLConnection() {
+        String hostName = "localhost";
+        String dbName = "mydb";
+        String userName = "root";
+        String password = "root";
+
+        try {
+            return getMySQLConnection(hostName, dbName, userName, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getMySQLConnection(String hostName, String dbName,
+                                                String userName, String password) throws SQLException,
+            ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+        Connection conn = DriverManager.getConnection(connectionURL, userName, password);
+        return conn;
+    }
 }
